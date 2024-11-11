fba34ba41caa2f1192cad30934d9ef0db8d5fba3
Juan Ignacio Frangolini <nachofrangolini@hotmail.com>
2024-11-11 14:29:16 -0300

    fix: reset the env to fix problems

--- mybotenv/Scripts/activate.bat
+++ mybotenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Nacho\Desktop\Bot Telegram\mybotenv
+set VIRTUAL_ENV=C:\Users\Nacho\Desktop\Bot Telegram\alert-price-bot\mybotenv
 
 if not defined PROMPT set PROMPT=$P$G
 
